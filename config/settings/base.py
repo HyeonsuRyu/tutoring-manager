a5d3279eabadb85c5aeac8ef7d9e39c5b06f08c7
Hyeonsu,Ryu <hsoo112149@gmail.com>
2026-05-23 04:14:36 +0900

    레이아웃 수정정

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,6 +165,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "data" / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "data" / "media"
 
--- tests/integration/accounts/test_totp.py
+++ tests/integration/accounts/test_totp.py
@@ -14,9 +14,13 @@ def test_totp_setup_page_when_logged_in(logged_in_client):
 def test_password_change_page_when_logged_in(logged_in_client):
     res = logged_in_client.get("/accounts/password/change/")
     assert res.status_code == 200
+    assert "app-shell" in res.content.decode()
 
 
 @pytest.mark.integration
 def test_email_page_when_logged_in(logged_in_client):
     res = logged_in_client.get("/accounts/email/")
     assert res.status_code == 200
+    html = res.content.decode()
+    assert "app-shell" in html
+    assert "등록된 이메일" in html
